d1e2715fdadc3be51d3f81bbd26ebe206f427f3e
dvjsm26 <dhananjayvs.m@gmail.com>
2017-05-31 14:24:38 +0530

    removed unnecessary lines

--- dirindex.py
+++ dirindex.py
@@ -17,30 +17,21 @@ def index_directory(dir):
 
 def index_file(fname,k):
 	with open(fname, 'r') as fp:
-		docstr = fp.read()
-		#docstr = docstr.replace("\\n", " ")
-		#docstr = docstr.replace("\\r", "")
-		document = json.loads(docstr)
+		document = json.load(fp)
 	
 	document["id"] = document.pop("_id")
 	document["type"] = document.pop("_type")
 	if("object_value" in document.keys()):
 		document["object_value"] = str(document["object_value"])	
 
-	# if(k>=13):
-	# 	print(json.dumps(document))
 	print("now indexing")
 	es.index(index='gstudio', doc_type='generic', id=document["id"]["$oid"], body=json.dumps(document))
 	print("indexed document %d" % k)
 
 def main():
-	print("creating object")
+	print("Starting the indexing process")
 
 	dir = '/home/dvjsm/Gstudio/data/rcs-repo/'
 	index_directory(dir)
 
-	# res = es.search(index='posts', doc_type='blog', body={"query": {"match": {"author": "Bill"}}})
-	# print("%d documents found" % res['hits']['total'])
-	# for doc in res['hits']['hits']:
-	#     print("%s) %s" % (doc['_id'], doc['_source']['content'])
 main()
