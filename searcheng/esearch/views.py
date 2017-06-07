7963a2a1b2ce80ac94756c820e6f4f0bb02c6fce
dvjsm26 <dhananjayvs.m@gmail.com>
2017-06-07 16:14:50 +0530

    Added algorithm for rescoring in search for better search results

--- searcheng/esearch/views.py
+++ searcheng/esearch/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from elasticsearch import Elasticsearch		
-import json
+import re
 
 from .forms import SearchForm
 
@@ -14,15 +14,34 @@ def get_search(request):
 		form = SearchForm(request.POST)
 		if form.is_valid():
 			query = form.cleaned_data['query']
-			query = request.POST['query']
+			print(query)
 			select = request.POST['select']
 			if(select=="all"):
 				select = ""
-			res = es.search(index="gstudio",doc_type=select, body={"query": {"multi_match": {
-														"query": query,
-														"type": "best_fields",
-														"fields": ["name^2", "altnames", "content"]
-				}}})
+
+			res = es.search(index="nroer",doc_type=select, body={"query": {
+																			"multi_match": {
+																				"query" : query,
+																				"type": "best_fields",
+																				"fields": ["name^2", "altnames", "content", "tags"],
+																				"minimum_should_match": "30%"
+																				}
+																			},
+																	"rescore": {
+																		"window_size": 50,
+																		"query": {
+																			"rescore_query": {
+																				"bool": {
+																					"should": [
+																						{"match_phrase": {"name": { "query": query, "slop":50}}},
+																						{"match_phrase": {"altnames": { "query": query, "slop": 50}}},
+																						{"match_phrase": {"content": { "query": query, "slop": 50}}}
+																					]
+																				}
+																			}
+																		}
+																	}
+																})
 			hits = "No of docs found: %d" % res['hits']['total']
 			res_list = ['Result :', hits]
 			med_list = []
@@ -36,9 +55,9 @@ def get_search(request):
 						l = s.index('.')
 					else:
 						l = len(s)
-					med_list.append([doc['_id'],s[0:l],doc['_source']['if_file']['original']['relurl']])	#printing only the id for the time being along with the node name
+					med_list.append([doc['_id'],s[0:l],doc['_source']['if_file']['original']['relurl'],doc['_score']])	#printing only the id for the time being along with the node name
 				else:
-					med_list.append([doc['_id'],doc['_source']['name'],None])
+					med_list.append([doc['_id'],doc['_source']['name'],None,doc['_score']])
 
 			return render(request, 'esearch/basic.html', {'header':res_list, 'content': med_list})
 
