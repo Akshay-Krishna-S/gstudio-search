759773be33e223309ae2a32cee187aa11df1679f
Vignesh-sopho <chiggy97.vn@gmail.com>
2017-06-19 11:37:16 +0530

    added filters frontend

--- searcheng/esearch/forms.py
+++ searcheng/esearch/forms.py
@@ -1,7 +1,18 @@
 from django import forms
-
+import os 
+import sys
 CHOICES=[("all",'All'),("Author",'Users'),("image",'Images'),("video",'Video'),("text",'Text'),("audio","Audio")]
+GROUP_CHOICES = [("all",'All')]
+
+f = open("esearch/group_list.txt","r")
+groups = f.readlines()
+for l in groups:
+	k = l.split()
+	tup = (k[1],k[1])
+	tup = tuple(tup)
+	GROUP_CHOICES.append(tup)
 
 class SearchForm(forms.Form):
 	query = forms.CharField(label = 'Query')
+	group_select = forms.ChoiceField(label = "Group:",widget = forms.Select, choices = GROUP_CHOICES)
 	select = forms.ChoiceField(widget=forms.Select, choices=CHOICES)
