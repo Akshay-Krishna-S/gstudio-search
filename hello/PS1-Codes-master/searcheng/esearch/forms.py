8a717cd66d0e6658cc7ba5b43c563981c9db69a9
ashwintayade10 <ashwintayade10@gmail.com>
2017-06-19 12:21:23 +0530

    Group Filter UI

--- hello/PS1-Codes-master/searcheng/esearch/forms.py
+++ hello/PS1-Codes-master/searcheng/esearch/forms.py
@@ -4,5 +4,5 @@ CHOICES=[("all",'All'),("Author",'Users'),("image",'Images'),("video",'Video'),(
 
 class SearchForm(forms.Form):
 	query = forms.CharField(label = 'Query')
-	group-select = forms.ChoiceField(label = "Group Select",widget=forms.Select, choices=GROUP_CHOICES)
+	group_select = forms.ChoiceField(label = "Group Select",widget=forms.Select, choices=GROUP_CHOICES)
 	select = forms.ChoiceField(label = "Filter",widget=forms.Select, choices=CHOICES)
