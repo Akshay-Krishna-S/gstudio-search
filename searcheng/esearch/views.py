d494b44f06ec3307d8311b5f2e54d15cd2feb323
dvjsm26 <dhananjayvs.m@gmail.com>
2017-06-20 13:20:22 +0530

    Added support for quotes in query

--- searcheng/esearch/views.py
+++ searcheng/esearch/views.py
@@ -8,11 +8,14 @@ es = Elasticsearch(['http://elsearch:changeit@localhost:9200'])
 author_map = {}
 f = open("esearch/author_mappings.txt","r")
 authors = f.readlines()
+
 for l in authors:
 	k = l.split(':')
 	auth_name = (k[1].split(';'))[1]
 	author_map[str(auth_name)] = k[0] #post gre sql ids
+
 f.close()
+
 def resources_in_group(res,group_select):
 	results = []
 	returnList = []
@@ -30,13 +33,6 @@ def resources_in_group(res,group_select):
 				if group_id == j["$oid"]:
 					results.append(i)
 
-	# for i in results:
-	# 	if('if_file' in i.keys()):
-	# 		s = i['name']
-	# 		if '.' in s
-	# 			l=s.index('.')
-	# 		else:
-	# 			l=len(s)
 	print("for groups")
 	print(results)
 	return results
@@ -81,75 +77,109 @@ def get_search(request):
 		# queryNameInfo[2],queryContentInfo[2],queryTagsInfo[2]="","",""								#the top search query with collate_match=true suggested by phsug_name, phsug_content, phsug_tags suggest bodies
 		# query_display_name,query_display_content,query_display_tags="","",""		
 
-		get_suggestion(phsug_name,queryNameInfo,select,query)
-		get_suggestion(phsug_content,queryContentInfo,select,query)
-		get_suggestion(phsug_tags,queryTagsInfo,select,query)
+		dqlis = [] 			# a list conatining all the text inserted within double quotes
+		q = "" 				# a string to hold the text not enclosed within ""
 
-		#print(res)
-		print (queryNameInfo[0],queryContentInfo[0],queryTagsInfo[0])
-		query_display = ""
+		#including quotes
+		if('"' in query):
+			l = re.split('(")', query)
+			qlist = list(filter(lambda a: a!='', l))
+			
+			itr = 0
+			while(itr<len(qlist)):
+				if(qlist[itr]=='"'):
+					if(itr+2<len(qlist) and qlist[itr+2]=='"'):
+						dqlis.append(qlist[itr+1])
+						itr+=2
+				else:
+					q += qlist[itr]
+				itr += 1
 
-		#what if all are 1 and 2/3 names are same but the third one has higher score
-		if((queryNameInfo[0]==1 and queryNameInfo[2]==query) or (queryContentInfo[0]==1 and queryContentInfo[2]==query) or (queryTagsInfo[0]==1 and queryTagsInfo[2]==query)): 
-			#if the original query is the query to be searched
+		print(dqlis, q)
+
+		#dealing with the case when the user has given "" in the query
+		if(len(dqlis)>0):
+			query_body = '{ "query": {"bool": { "should": ['
+			for quot in dqlis:
+				query_body += ('{"multi_match": {"query": "%s", "fields": ["name^3", "altnames", "content^2", "tags"], "type": "phrase"}},' % (quot))
+			if(q!=''):
+				query_body += ('{"multi_match": {"query": "%s", "fields": ["name^3", "altnames", "content^2", "tags"], "type": "best_fields"}},' % (q))
+			query_body += (']}}}')
+
+			res = es.search(index='nroer_pro', doc_type=select, body=eval(query_body))	
 			query_display = query
-		elif(queryNameInfo[0]==0 and queryContentInfo[0]==0 and queryTagsInfo[0]==0):																		
-			#if we didnt find any suggestion, neither did we find the query already indexed->query remains same
-			query_display = query
-		else: #if we found a suggestion 
-			res1_list = ['Search instead for <a href="">%s</a>'%(query)] #if the user still wants to search for the original query he asked for
-			if(queryNameInfo[1]>=queryContentInfo[1] and queryNameInfo[1]>=queryTagsInfo[1]):						 #comparing the scores of name,content,tags suggestions and finding the max of the three
-				query = queryNameInfo[2]
-				query_display = queryNameInfo[3]					 #what query to display on the search result screen
-			if(queryContentInfo[1]>queryNameInfo[1] and queryContentInfo[1]>=queryTagsInfo[1]):
-				query = queryContentInfo[2]
-				query_display = queryContentInfo[3]
-			if(queryTagsInfo[1]>queryContentInfo[1] and queryTagsInfo[1]>queryNameInfo[1]):
-				query = queryTagsInfo[2]
-				query_display = queryTagsInfo[3]
 
-		if(queryNameInfo[0]==0 and queryContentInfo[0]==0 and queryTagsInfo[0]==0):#if we didnt find any suggestion, neither did we find the query already indexed
-			res = es.search(index="nroer_pro",doc_type=select, body={"query": {
-																		"multi_match": { 											#first do a multi_match
-																			"query" : query,
-																			"type": "best_fields",									#when multiple words are there in the query, try to search for those words in a single field
-																			"fields": ["name^3", "altnames", "content^2", "tags"],	#in which field to search the query
-																			"minimum_should_match": "30%"
-																			}
-																		},
-																	"rescore": {													#rescoring the top 50 results of multi_match
-																		"window_size": 50,
-																		"query": {
-																			"rescore_query": {
-																				"bool": {											#rescoring using match phrase
-																					"should": [
-																						{"match_phrase": {"name": { "query": query, "slop":2}}},
-																						{"match_phrase": {"altnames": { "query": query, "slop": 2}}},
-																						{"match_phrase": {"content": { "query": query, "slop": 4}}}
-																					]
+		else:
+
+			get_suggestion(phsug_name, queryNameInfo, select, query)
+			if(queryNameInfo[2]!=query):
+				get_suggestion(phsug_content, queryContentInfo, select, query)
+			if(queryNameInfo[2]!=query and queryContentInfo[2]!=query):
+				get_suggestion(phsug_tags, queryTagsInfo, select, query)
+
+			print (queryNameInfo[0],queryContentInfo[0],queryTagsInfo[0])
+			query_display = ""
+
+			#what if all are 1 and 2/3 names are same but the third one has higher score
+			if((queryNameInfo[0]==1 and queryNameInfo[2]==query) or (queryContentInfo[0]==1 and queryContentInfo[2]==query) or (queryTagsInfo[0]==1 and queryTagsInfo[2]==query)): 
+				#if the original query is the query to be searched
+				query_display = query
+			elif(queryNameInfo[0]==0 and queryContentInfo[0]==0 and queryTagsInfo[0]==0):																		
+				#if we didnt find any suggestion, neither did we find the query already indexed->query remains same
+				query_display = query
+			else: #if we found a suggestion 
+				res1_list = ['Search instead for <a href="">%s</a>'%(query)] #if the user still wants to search for the original query he asked for
+				if(queryNameInfo[1]>=queryContentInfo[1] and queryNameInfo[1]>=queryTagsInfo[1]):						 #comparing the scores of name,content,tags suggestions and finding the max of the three
+					query = queryNameInfo[2]
+					query_display = queryNameInfo[3]					 #what query to display on the search result screen
+				if(queryContentInfo[1]>queryNameInfo[1] and queryContentInfo[1]>=queryTagsInfo[1]):
+					query = queryContentInfo[2]
+					query_display = queryContentInfo[3]
+				if(queryTagsInfo[1]>queryContentInfo[1] and queryTagsInfo[1]>queryNameInfo[1]):
+					query = queryTagsInfo[2]
+					query_display = queryTagsInfo[3]
+
+			if(queryNameInfo[0]==0 and queryContentInfo[0]==0 and queryTagsInfo[0]==0):#if we didnt find any suggestion, neither did we find the query already indexed
+				res = es.search(index="nroer_pro",doc_type=select, body={"query": {
+																			"multi_match": { 											#first do a multi_match
+																				"query" : query,
+																				"type": "best_fields",									#when multiple words are there in the query, try to search for those words in a single field
+																				"fields": ["name^3", "altnames", "content^2", "tags"],	#in which field to search the query
+																				"minimum_should_match": "30%"
+																				}
+																			},
+																		"rescore": {													#rescoring the top 50 results of multi_match
+																			"window_size": 50,
+																			"query": {
+																				"rescore_query": {
+																					"bool": {											#rescoring using match phrase
+																						"should": [
+																							{"match_phrase": {"name": { "query": query, "slop":2}}},
+																							{"match_phrase": {"altnames": { "query": query, "slop": 2}}},
+																							{"match_phrase": {"content": { "query": query, "slop": 4}}}
+																						]
+																					}
 																				}
 																			}
 																		}
-																	}
-																})
+																	})
 
-		else: #if we found a suggestion or if the query exists as a phrase in one of the name/content/tags field
-			res = es.search(index="nroer_pro",doc_type=select, body={"query": {
-																		"multi_match": {
-																			"query": query,
-																			"fields": ["name^3", "altnames", "content^2", "tags"],
-																			"type": "phrase", #we are doing a match phrase on multi field.
-																			"slop": 5
+			else: #if we found a suggestion or if the query exists as a phrase in one of the name/content/tags field
+				res = es.search(index="nroer_pro",doc_type=select, body={"query": {
+																			"multi_match": {
+																				"query": query,
+																				"fields": ["name^3", "altnames", "content^2", "tags"],
+																				"type": "phrase", #we are doing a match phrase on multi field.
+																				"slop": 5
+																			}
 																		}
-																	}
-																})
+																	})
 
 		if group_select!="all"and select!="Author":
 			restrial = resources_in_group(res,group_select)
 
 		hits = "No of docs found: %d" % res['hits']['total']
 		res_list = ['Showing results for %s :' % query_display, hits]
-		res_list = ['Showing results for %s :' % query_display]
 		#med_list is the list which will be passed to the html file.
 		med_list = []
 		for doc in res['hits']['hits']:					
@@ -166,8 +196,8 @@ def get_search(request):
 		if(len(res1_list)>0):
 			return render(request, 'esearch/basic.html', {'header':res_list, 'alternate': res1_list, 'content': med_list})
 		return render(request, 'esearch/basic.html', {'header':res_list, 'content': med_list})
-		return render()
-#if the search page is loaded for the first time
+
+	#if the search home page is being requested
 	else:
 		form = SearchForm()
 
@@ -218,7 +248,6 @@ def get_suggestion_body(query,field_value,slop_value,field_name_value):
 	phrase_suggest = {												#json body of phrase suggestion in name field
 		"suggest": {
 			"text": query,										#the query for which we want to find suggestion
-			#"simple_phrase": {
 				"phrase": {											
 					"field": field_value,					#in which indexed field to find the suggestion
 					"gram_size": 3,								#this is the max shingle size
@@ -254,7 +283,7 @@ def get_suggestion_body(query,field_value,slop_value,field_name_value):
 	return phrase_suggest
 
 
-def get_suggestion(suggestion_body,queryInfo,doc_types,query):
+def get_suggestion(suggestion_body, queryInfo, doc_types, query):
 	res = es.suggest(body=suggestion_body, index='nroer_pro')						#first we search for suggestion in the name field as it has the highest priority
 	print(res)																					
 	if(len(res['suggest'][0]['options'])>0):									#if we get a suggestion means the phrase doesnt exist in the index
@@ -264,47 +293,9 @@ def get_suggestion(suggestion_body,queryInfo,doc_types,query):
 				queryInfo[1] = sugitem['score']
 				queryInfo[2] = sugitem['text']				
 				queryInfo[3] = sugitem['highlighted']						#the query to be displayed onto the search results screen
-				#print(res)
 				break
 	else:						#should slop be included in the search part here?
 		query_body = {"query":{"match_phrase":{"name": query,}}}
 		if(es.search(index='nroer_pro',doc_type=doc_types,body=query_body)['hits']['total']>0):
 			queryInfo[0] = 1							#set queryNameInfo[0] = 1 when we found a suggestion or we found a hit in the indexed data
 			queryInfo[2] = query
-
-
-
-
-# phsug_content = {											#json body of phrase suggestion in content field
-# 	"suggest": {
-# 		"text": query,
-# 		"phrase": {
-# 				"field": "content.trigram",
-# 				"gram_size": 3,
-# 				"max_errors": 2,
-# 				"direct_generator": [ {
-# 		          "field": "content.trigram",
-# 		          "min_word_length": 2,
-# 				  "prefix_length": 0,
-# 		          "suggest_mode": "missing"
-# 		        } ],
-# 		        "highlight": {
-# 		          "pre_tag": "<em>",
-# 		          "post_tag": "</em>"
-# 		        },
-# 		        "collate": {
-# 		        	"query": {
-# 		        		"inline": {
-# 		        			"match_phrase": {
-# 		        				"{{field_name}}": {
-# 			        				"query": "{{suggestion}}",
-# 			        				"slop": 3
-# 			        			}
-# 		        			}
-# 		        		}
-# 		        	},
-# 		        	"params": {"field_name": "content"},
-# 		        	"prune": True
-# 		        }
-# 			},
-# 		}
